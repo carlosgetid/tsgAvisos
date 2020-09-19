000e9640defa1c2e1d5e8aa32a1fc931de10a041
paulable <carlospgp13@gmail.com>
2020-09-19 17:28:53 -0500

    sp buscar usuario para login

--- sistemaAvisosTSG/App_Start/RouteConfig.cs
+++ sistemaAvisosTSG/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace sistemaAvisosTSG
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Hacdata", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Usuarios", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
